e27dde49c1d664a3869a54c1130cf90be39d4637
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-04-28 20:04:24 +0530

    feat: Support to receive files over HTTPS protocol

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+  config: { destination: '.', port: 4000 },
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs-extra';
+import config from './config';
+
+const app = express();
+const port = 3000;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(req.body);
+    const dest = `./uploads/${req.body.exam}/${req.body.id}`;
+    fs.mkdirsSync(dest);
+    cb(null, dest);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const upload = multer({ storage });
+// Save file
+app.post('/submit', upload.any(), (req, res) => res.send('Data saved'));
+
+app.listen(config.port, () => {
+  console.log(`Example app listening at http://${app.get('host')}:${port}`);
+});
