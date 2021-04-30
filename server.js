fb18c5e94820408703e042f46238852f9324642d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-04-30 20:55:45 +0530

    feat: Add error handling for uploads

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ import fs from 'fs-extra';
 import config from './config.js';
 
 const app = express();
-const port = 3000;
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		console.log(req.body);
-		const dest = `./uploads/${req.body.exam}/${req.body.id}`;
+		const dest = `${config.destination}/${req.body.exam}/${req.body.id}`;
 		fs.mkdirsSync(dest);
 		cb(null, dest);
 	},
@@ -23,10 +22,25 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-const upload = multer({ storage });
 // Save file
-app.post('/submit', upload.any(), (req, res) => res.send('Data saved'));
+app.post('/submit', (req, res) => {
+	const upload = multer({ storage });
+	upload(req, res, (err) => {
+		if (req.fileValidationError) {
+			return res.send(req.fileValidationError);
+		} else if (!req.file) {
+			return res.send('Please select a valid file.');
+		} else if (err instanceof multer.MulterError) {
+			return res.send(err);
+		} else if (err) {
+			return res.send(err);
+		} else if (config.portals.indexOf(req.body.exam) === -1) {
+			res.send('Please select a valid portal for submission');
+		}
+	});
+	res.send('Data saved');
+});
 
 app.listen(config.port, () => {
-	console.log(`Hermes listening at http://localhost:${port}`);
+	console.log(`Hermes listening at http://localhost:${config.port}`);
 });
