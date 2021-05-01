0be71f0118255a95c12497f596fbe1e1cb3060fa
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2021-05-01 12:03:15 +0530

    feat: Add questionnaire for file submission

--- client.js
+++ client.js
@@ -2,7 +2,11 @@
 import axios from 'axios';
 import { Command } from 'commander/esm.mjs';
 import chalk from 'chalk';
+import inquirer from 'inquirer';
 const program = new Command();
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
+
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
 
 program
 	.requiredOption('-i --ip <ip>', 'IP address of the portal server')
@@ -14,44 +18,32 @@ console.log(chalk.blue('Fetching config from server...'));
 axios.get(`http://${args.ip}:${args.port}/config`).then((res) => {
 	console.log(res.data);
 	console.log(chalk.green('Config fetched'));
+
+	// Ask question as submit the file
+	inquirer
+		.prompt([
+			{
+				name: 'portal',
+				message: 'Choose the portal you want to make the submission to',
+				choices: res.data.portals,
+				type: 'list',
+			},
+			{
+				name: 'filePath',
+				message: 'Enter the the path of the file you want to submit',
+				type: 'file-tree-selection',
+			},
+			{
+				name: 'id',
+				message: 'What is your unique ID?',
+			},
+			{
+				name: 'confirm',
+				message: 'Do you want to submit the file?',
+				type: 'confirm',
+			},
+		])
+		.then((answers) => {
+			console.log(answers);
+		});
 });
-
-// inquirer
-// 	.prompt([
-// 		{
-// 			name: 'faveReptile',
-// 			message: 'What is your favorite reptile?',
-// 			default: 'Alligators',
-// 		},
-// 		{
-// 			name: 'faveColor',
-// 			message: 'What is your favorite color?',
-// 			default: '#008f68',
-// 		},
-// 	])
-// 	.then((answers) => {
-// 		ans = answers;
-// 	});
-
-/*
-	Create the config
-	-> Ask for the number of submit portals
-	-> Ask for the id of each submit portals
-	-> Ask for the port
-	-> Ask for the destination directory
-	-> Create the config file
-*/
-
-// const portals = [];
-// const config = {};
-
-// inquirer.prompt([
-// 	{
-// 		name: 'numberOfPortals',
-// 		message: 'How many submit portals do you want to create?',
-// 		type: Number,
-// 	},
-// 	{
-
-// 	}
-// ]);
